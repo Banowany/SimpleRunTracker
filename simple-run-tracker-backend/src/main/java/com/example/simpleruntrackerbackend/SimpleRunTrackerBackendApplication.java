be1e4dee89293ca284be400ce0c2c8f2cd33dbdc
Krzysztof Gebka <krzysztof.gebka2001@gmail.com>
2025-01-02 14:11:37 +0100

    Create empty backend in spring

---
+++ simple-run-tracker-backend/src/main/java/com/example/simpleruntrackerbackend/SimpleRunTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleruntrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRunTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRunTrackerBackendApplication.class, args);
+    }
+
+}
