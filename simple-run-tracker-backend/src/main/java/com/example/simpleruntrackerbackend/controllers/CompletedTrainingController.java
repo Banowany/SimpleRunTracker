344621a3a8b02c876b019e6e4161790c7cd03527
Krzysztof Gebka <krzysztof.gebka2001@gmail.com>
2025-01-12 16:52:47 +0100

    Implement TrainingService in front for API interactions

--- simple-run-tracker-backend/src/main/java/com/example/simpleruntrackerbackend/controllers/CompletedTrainingController.java
+++ simple-run-tracker-backend/src/main/java/com/example/simpleruntrackerbackend/controllers/CompletedTrainingController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/trainings")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CompletedTrainingController {
     private final CompletedTrainingService completedTrainingService;
 
